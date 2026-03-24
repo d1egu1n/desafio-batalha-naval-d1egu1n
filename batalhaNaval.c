f6073c9c754d68661295e22d118a3d97dc88001f
Edson Diego <124009790+d1egu1n@users.noreply.github.com>
2026-03-24 13:36:20 -0300

    implementa posicionamento de navios no tabuleiro (nível novato)
    - Cria matriz 10x10 representando o tabuleiro inicializado com 0
    - Implementa dois navios (horizontal e vertical) com tamanho fixo 3
    - Utiliza vetores unidimensionais para representar os navios
    - Adiciona validação de limites do tabuleiro
    - Garante que não haja sobreposição entre navios
    - Separa lógica de validação e inserção
    - Exibe o tabuleiro formatado no console com identificação de linhas e colunas

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,6 +7,98 @@
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
+    char linha [10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+    int tabuleiro [10][10];
+    
+    // 1. Inicializar tabuleiro com 0
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+
+            tabuleiro[i][j] = 0;
+        }
+        
+    }
+
+    // 2. vetores do navio
+    int navioH[3] = {3, 3, 3};
+    int navioV[3] = {3, 3, 3};
+
+    // 3. coordenadas do navio
+    int linhaH = 2;     int linhaV = 5;
+    int colunaH = 4;    int colunaV = 7;
+
+    // 4. validar o navio na horizontal
+    int podeH = 1;
+
+    if (colunaH + 3 > 10) {
+        podeH = 0;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        
+        if (tabuleiro[linhaH][colunaH + i] != 0) {
+            podeH = 0;
+        }
+        
+    }
+    
+    // 5. inserir navio horizontal
+      
+    if (podeH) {
+        for (int i = 0; i < 3; i++) {
+            tabuleiro[linhaH][colunaH + i] = navioH[i];
+        }
+    } else {
+        printf("Navio horizontal fora do limite!\n");
+    }
+     
+    // 6. validar navio na vertical
+    int podeV = 1;
+
+    if (linhaV + 3 > 10) {
+        podeV = 0;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        
+        if (tabuleiro[linhaV + i][colunaV] != 0){
+            podeV = 0;
+        }
+        
+    }
+      
+    // 7. inserir navio vertical
+
+    if (podeV) {
+        for (int i = 0; i < 3; i++) {
+            tabuleiro[linhaV + i][colunaV] = navioV[i];
+        }
+    } else {
+        printf("Navio vertical fora do limite!\n");
+    }
+
+    // 8. Imprimir topo (colunas)
+    printf("    ");
+    for (int i = 0; i < 10; i++) {
+        printf("%c  ", linha[i]);
+    }
+    printf("\n");
+
+    // 9. Imprimir tabuleiro com numeração à esquerda
+    for (int i = 0; i < 10; i++) {
+
+    printf("%2d ", i + 1); // lado esquerdo
+
+    for (int j = 0; j < 10; j++) {
+        printf("%2d ", tabuleiro[i][j]);
+    }
+
+    printf("\n");
+    }
+
+    
+
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
